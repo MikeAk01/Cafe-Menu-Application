45e5a2a672a98cf5af9497efbd3490540d7cee7e
DukeDennis <P2666957@my365.dmu.ac.uk>
2024-01-03 00:32:37 +0000

    Still can get customer add item card to not work kmtt

---
+++ app/src/main/java/com/example/cafemobileapplication/Customer/CustomerMenu.kt
@@ -0,0 +1,14 @@
+package com.example.cafemobileapplication.Customer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cafemobileapplication.R
+
+class CustomerMenu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_browse)
+    }
+
+
+}
--- app/src/main/java/com/example/cafemobileapplication/Customer/DisplayCustomerHome.kt
+++ app/src/main/java/com/example/cafemobileapplication/Customer/DisplayCustomerHome.kt
@@ -1,13 +1,46 @@
 package com.example.cafemobileapplication.Customer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cafemobileapplication.R
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class DisplayCustomerHome : AppCompatActivity() {
+
+    //declaring variables
+    lateinit var c_add_items: CardView
+    lateinit var c_browse_menu: CardView
+    lateinit var c_modify_order: CardView
+    lateinit var c_confirmation: CardView
+    lateinit var c_write_feedback: CardView
+    lateinit var c_logout: CardView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_customer_home)
+
+        //Create a listener when the user click one of the cards
+        //view menu
+        c_browse_menu = findViewById(R.id.Customer_menu_card)
+        c_browse_menu.setOnClickListener{
+            var send = Intent(this, CustomerMenu::class.java)
+            startActivity(send)
+        }
+
+
+
     }
 
+
+
+
 }
+
