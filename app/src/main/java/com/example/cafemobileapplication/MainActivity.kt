11780afe806941f6750d39bfeea1f154b3e87bd3
DukeDennis <P2666957@my365.dmu.ac.uk>
2024-01-16 16:46:45 +0000

    fixed the home page now... onto doing the logout function.

--- app/src/main/java/com/example/cafemobileapplication/MainActivity.kt
+++ app/src/main/java/com/example/cafemobileapplication/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.cafemobileapplication
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.cafemobileapplication.Admin.AdminLogin
 import com.example.cafemobileapplication.Customer.CustomerLogin
-import com.example.cafemobileapplication.Customer.DisplayCustomerHome
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         //Instantiate the button widgets
         val btnCustomer: Button = findViewById(R.id.btnCustomer)
         val btnAdmin:Button = findViewById(R.id.btnAdmin)
+
 
         //Start the Customer Home page activity when admin is clicked
         btnCustomer.setOnClickListener{
@@ -32,5 +31,9 @@ class MainActivity : AppCompatActivity() {
             startActivity(send)
         }
 
+
+
     }
+
+
 }
